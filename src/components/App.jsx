cf3225cf448a421aad10f85e92ca5f7d9c0d4ea9
Tomek Cekałą <126020134+tomcek2@users.noreply.github.com>
2023-11-23 20:13:05 +0100

    Searchbar done
    Searchbar component:
    - Created Searchbar class that renders header with searchbar form.
    - Added handleChange class method whitch saves value of input to state,
    - Added handleSubmit class method that calls the onSubmit function in
    the App component and passing the current search query as an argument,

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,51 @@
-export const App = () => {
-  return <div></div>;
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Searchbar } from './Searchbar/Searchbar';
+
+const API_KEY = '39394747-fa1f159daeb12a76c3032126a';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    isLoading: false,
+    showModal: false,
+    modalImage: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
+    }
+  }
+
+  handleSearchSubmit = query => {
+    this.setState({ query, page: 1, images: [] });
+  };
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+    const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+    axios
+      .get(url)
+      .then(response => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...response.data.hits],
+        }));
+      })
+      .catch(error => {
+        console.error('Error fetching images:', error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import style from 'components/styles.module.css';
+
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
+
+  render() {
+    return (
+      <header className={style.Searchbar}>
+        <form className={style.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={style.SearchFormButton}>
+            <span className={style.SearchFormButtonLabel}>Search</span>
+          </button>
+          <input
+            className={style.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
